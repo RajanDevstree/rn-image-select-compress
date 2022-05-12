a269b325b129e74f96bbe8ad0144845e72e0ae00
RajanDevstree <rajan.devstree@gmail.com>
2022-05-12 19:43:42 +0530

    code add

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # rn-image-select-compress
+
+###
+
+react-native-image-picker and react-native-image-resizer library first install
+
+```
+import RnImageSelectCompress from 'rn-image-select-compress';
+
+RnImageSelectCompress().then(data => {
+    console.log(data);
+});
+
+
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+import { launchImageLibrary } from "react-native-image-picker";
+import ImageResizer from "react-native-image-resizer";
+
+const RnImageSelectCompress = async (
+  width = 0,
+  height = 0,
+  compressFormat = "JPEG",
+  quality = 50
+) => {
+  let options = {
+    title: "Select Image",
+    customButtons: [
+      {
+        name: "customOptionKey",
+        title: "Choose Photo from Custom Option",
+      },
+    ],
+    storageOptions: {
+      skipBackup: true,
+      path: "images",
+    },
+  };
+  const responseImage = await launchImageLibrary(options);
+  if (responseImage.didCancel) {
+    return responseImage.didCancel;
+  } else if (responseImage.error) {
+    return responseImage.error;
+  } else if (responseImage.customButton) {
+    return responseImage.customButton;
+  } else {
+    let source = responseImage;
+    const responseImageCompress = await ImageResizer.createResizedImage(
+      source["assets"][0].uri,
+      width == 0 ? source["assets"][0].width : width,
+      height == 0 ? source["assets"][0].width : height,
+      compressFormat,
+      quality,
+      0,
+      null
+    );
+
+    responseImageCompress["oldSize"] = source["assets"][0].fileSize;
+    return responseImageCompress;
+  }
+};
+
+export default RnImageSelectCompress;
